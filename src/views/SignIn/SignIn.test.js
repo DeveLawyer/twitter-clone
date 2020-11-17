fe0e247342710048bbba5c4777a4b975bf262392
Josue Narro <josue.narro.a@gmail.com>
2020-11-17 09:13:18 -0500

    Create SignInTest

---
+++ src/views/SignIn/SignIn.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import SignIn from "./SignIn";
+
+describe("SignIn", () => {
+  it("true is truthy", () => {
+    expect(true).toBe(true);
+  });
+});
